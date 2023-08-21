996a71fc446dd3688377b7fde07a677f044a7c29
amirahad <arahad4783@gmail.com>
2023-08-21 23:17:24 +0600

    first commit

---
+++ app/(frontend)/components/common/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p className='header_1' >
+                Header
+            </p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ app/(frontend)/components/home/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='container' >
+            test
+        </div>
+    );
+};
+
+export default Hero;
